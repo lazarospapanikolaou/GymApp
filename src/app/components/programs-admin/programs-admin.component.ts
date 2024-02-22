abb99426b7926af62bf981b982556ce68076460a
LazarosPap <l.papanikolaou@pragma-iot.com>
2024-02-22 13:00:25 +0200

    update

--- src/app/components/programs-admin/programs-admin.component.ts
+++ src/app/components/programs-admin/programs-admin.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-programs-admin',
   standalone: true,
-  imports: [CardModule],
+  imports: [CardModule, ButtonModule],
   templateUrl: './programs-admin.component.html',
   styleUrl: './programs-admin.component.scss'
 })
