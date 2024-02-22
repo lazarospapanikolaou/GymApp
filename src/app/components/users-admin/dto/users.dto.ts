19a482e254155f0b3520fbabd2557a989ec62eb6
chrisboursis <chrisboursis@gmail.com>
2024-02-22 12:45:49 +0200

    Working on users-admin page

---
+++ src/app/components/users-admin/dto/users.dto.ts
@@ -0,0 +1,7 @@
+export class UsersDto {
+    id: number;
+    userName: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+}
--- src/app/components/users-admin/users-admin.component.ts
+++ src/app/components/users-admin/users-admin.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersAdminService } from '../../service/users-admin.service';
+import { UsersDto } from './dto/users.dto';
+import { Table, TableModule } from 'primeng/table';
 
 @Component({
-  selector: 'app-users-admin',
-  standalone: true,
-  imports: [],
-  templateUrl: './users-admin.component.html',
-  styleUrl: './users-admin.component.scss'
+    selector: 'app-users-admin',
+    standalone: true,
+    imports: [TableModule],
+    templateUrl: './users-admin.component.html',
+    styleUrl: './users-admin.component.scss',
 })
-export class UsersAdminComponent {
+export class UsersAdminComponent implements OnInit {
+    users: UsersDto[] = [];
+    loading: boolean = true;
 
+    constructor(private users_admin_service: UsersAdminService) {}
+
+    ngOnInit(): void {
+        this.users_admin_service
+            .getAdminUsers()
+            .subscribe((res: UsersDto[]) => {
+                this.users = res;
+                console.log(this.users);
+                this.loading = false;
+            });
+    }
+
+    clear(table: Table) {
+        table.clear();
+    }
 }
