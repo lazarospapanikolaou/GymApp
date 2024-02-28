91339cd080560c02f6a32d70bc6ddb226b9fc317
LazarosPap <l.papanikolaou@pragma-iot.com>
2024-02-28 16:49:59 +0200

    Update

--- src/app/components/programs-admin/programs-admin.component.ts
+++ src/app/components/programs-admin/programs-admin.component.ts
@@ -4,37 +4,39 @@ import { CardModule } from 'primeng/card';
 import { Router } from '@angular/router';
 import {DialogService} from 'primeng/dynamicdialog';
 import {DynamicDialogRef} from 'primeng/dynamicdialog';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { AddNewProgramDialogComponent } from '../dialogs/add-new-program-dialog/add-new-program-dialog.component';
 
 @Component({
     selector: 'app-programs-admin',
     standalone: true,
-    imports: [CardModule, ButtonModule],
+    imports: [CardModule, ButtonModule, ToastModule, AddNewProgramDialogComponent],
     templateUrl: './programs-admin.component.html',
     styleUrl: './programs-admin.component.scss',
-    providers: [DialogService]
+    providers: [DialogService, MessageService]
 })
 export class ProgramsAdminComponent {
     showUsers: boolean = false;
     ref: DynamicDialogRef;
 
-    constructor(private router: Router, private dialogService: DialogService) {}
+    constructor(private router: Router, private dialogService: DialogService, private messageService: MessageService) {}
 
     changePage() {
         this.router.navigate(['/pages/users']);
     }
 
     createNewProgram(): void{
-        this.ref = this.dialogService.open(AddNewProgramDialog, {
-            header: 'Create a new Programm',
+        this.ref = this.dialogService.open(AddNewProgramDialogComponent, {
+            header: 'Create a new Program',
             width: '70%',
             contentStyle:  {"overflow": "auto"},
-            maximizable: true
+            maximizable: false
         });
 
         this.ref.onClose.subscribe((res) => {
             if (typeof res !== 'undefined') {
-                var message = '';
-                this.showToast
+                this.messageService.add({ severity:'error', summary: 'Error', detail: 'Faild to add a program'});
             }
         })
     }
